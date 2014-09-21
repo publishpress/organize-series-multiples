10059104514a5468f522977714550518220f3434
Darren Ethier <darren@roughsmootheng.in>
2014-09-21 16:39:38 -0400

    fix invalid extended method signature [touch: 167]
    also a lot of empty whitespace at eol cleared up.

--- os-multi-setup.php
+++ os-multi-setup.php
@@ -9,7 +9,7 @@
 
 //TODO
 /*
-* need to make sure that we show a warning on install that let's people know that installing this plugin is irreversible (in other words they can't go from multiple series back to Organize Series Core. 
+* need to make sure that we show a warning on install that let's people know that installing this plugin is irreversible (in other words they can't go from multiple series back to Organize Series Core.
 */
 if (!class_exists('osMulti') ) {
 
@@ -19,20 +19,20 @@ class osMulti {
 	var $version = OS_MULTI_VER;
 	var $os_multi_domain = 'organize-series-multiples';
 	var $message_id = 1;
-	
+
 	//__constructor
 	function osMulti() {
-		
+
 		// always check to make sure organize series is running first //
 		add_action('plugins_loaded', array(&$this, 'orgseries_check'));
-		
+
 		//initial setup stuff
 		add_action('activate_organize-series-multiples/organize-series-multiples.php', array(&$this, 'install'));
 		add_action('plugins_loaded', array(&$this, 'add_settings'));
 		add_action('init', array(&$this, 'register_textdomain'));
 		add_action('admin_enqueue_scripts', array(&$this, 'register_scripts_styles'));
 		add_action('admin_init', array($this, 'load_custom_columns'));
-		
+
 		//replacing organize series hooks/filters
 		add_action('quick_edit_custom_box', array(&$this, 'inline_edit'), 9, 2);
 		add_action('admin_print_scripts-edit.php', array(&$this, 'inline_edit_js'));
@@ -42,11 +42,11 @@ class osMulti {
 		add_action('wp_ajax_add_series', array(&$this, 'ajax_series'));
 		add_action('add_meta_boxes', array(&$this, 'meta_box'));
 		add_action('delete_series', array(&$this, 'delete_series'), 10, 2);
-		
+
 		//hooking into Organize Series
 		add_filter('orgseries_part_key', array(&$this, 'part_key'), 10, 2);
 		add_filter('orgseries_sort_series_page_where', array(&$this, 'sort_series'));
-		
+
 	}
 
 	function load_custom_columns() {
@@ -57,14 +57,14 @@ class osMulti {
 			add_action($filter_ref, array($this, 'posts_custom_column_action'), 12, 2);
 		}
 	}
-	
+
 	function orgseries_check() {
 		if (!class_exists('orgSeries') ) {
 			add_action('admin_notices', array(&$this, 'orgseries_warning'));
 			add_action('admin_notices', array(&$this, 'orgseries_deactivate'));
 			return;
 		}
-		
+
 		$os_version = get_option('org_series_version');
 		if ( $os_version < '2.3' ) {
 			$this->message_id = 2;
@@ -73,33 +73,33 @@ class osMulti {
 		}
 		return;
 	}
-	
+
 	function orgseries_warning() {
 		if ( $this->message_id == 1 ) {
 			$msg = '<div id="wpp-message" class="error fade"><p>'.__('The <strong>Organize Series Multiples</strong> addon for Organize Series requires the Organize Series plugin to be installed and activated in order to work.  Addons won\'t activate until this condition is met.', 'organize-series-multiples').'</p></div>';
 		}
-		
+
 		if ( $this->message_id == 2 ) {
 			$msg = '<div id="wpp-message" class="error fade"><p>' .__('The <strong>Organize Series Multiples</strong> addon for Organize Series requires version 2.3 or greater of Organize Series to be installed and activated in order to work.  Organize Series Multiples has been deactivated.', 'organize-series-multiples').'</p></div>';
 		}
 		echo $msg;
 	}
-	
+
 	function orgseries_deactivate() {
 		deactivate_plugins('organize-series-multiples/organize-series-multiples.php', true);
 	}
-	
+
 	function install() {
 		//let's do a version check (and make sure if this is version 0.1 that we do the update
 		$old_version = get_option('os_multi_version');
-		
+
 		if ( empty($old_version) || $this->version == '0.1' ) {
 			if ( $old_version != $this->version || $this->version == '0.1' ) $this->update($oldversion);
 		}
-		
+
 		update_option('os_multi_version', $this->version);
 	}
-	
+
 	function register_scripts_styles() {
 		$url = OS_MULTI_URL.'js/';
 		$c_url = OS_MULTI_URL.'css/';
@@ -112,7 +112,7 @@ class osMulti {
 			));
 		wp_enqueue_style('series-multiples-inline-edit', $c_url.'series-multiples-edit-php.css');
 	}
-	
+
 	//do stuff that needs to be done with a version upgrade (if necessary)
 	function update($version) {
 		global $wpdb;
@@ -120,7 +120,7 @@ class osMulti {
 			$import_chk = get_option('os_multi_import');
 			if ( empty($import_chk) || !$import_chk ) {
 				//this means we need to do an import to convert the SERIES_PART meta_key's in organize Series core to the SERIES_PART key for multiples (not reversible!)
-				
+
 				//get list of posts that contain the meta key SERIES_PART_KEY
 				$query = "SELECT p.ID, t.term_id, pm.meta_value FROM $wpdb->posts AS p LEFT JOIN $wpdb->postmeta AS pm ON p.ID = pm.post_id LEFT JOIN $wpdb->term_relationships AS tr ON p.ID = tr.object_id LEFT JOIN $wpdb->term_taxonomy AS tt ON tr.term_taxonomy_id = tt.term_taxonomy_id LEFT JOIN $wpdb->terms AS t ON tt.term_id = t.term_id WHERE pm.meta_key = '".SERIES_PART_KEY."' AND tt.taxonomy = 'series'";
 				$posts = $wpdb->get_results($query);
@@ -136,22 +136,22 @@ class osMulti {
 		}
 		return;
 	}
-	
+
 	function add_settings() {
 		//we'll use this for setting up the defaults for the os_multi options.  However there are none yet so let's get out of here.
 		return;
 	}
-	
+
 	function register_textdomain() {
 		$dir = basename(dirname(__FILE__)).'/lang';
 		load_plugin_textdomain('organize-series-multiples', false, $dir);
 	}
-	
+
 	function part_key($part_key, $series_id) {
 		$key = $part_key.'_'.$series_id;
 		return $key;
 	}
-	
+
 	function inline_edit( $column_name, $type ) {
 		//'organize-series-multiples'
 	$posttypes = apply_filters('orgseries_posttype_support', array('post') );
@@ -165,25 +165,25 @@ class osMulti {
 				</ul>
 				<input type="hidden" name="series_post_id" class="series_post_id"  />
 				<input type="hidden" name="is_series_save" value="1" />
-			
-			
+
+
 		</div></div></fieldset>
 			<?php
 		}
 	}
-	
+
 	function inline_edit_js() {
 		wp_enqueue_script('inline-edit-series-multiples');
 	}
-	
+
 	function add_series_js() {
 		wp_enqueue_script('series-multiples-add');
 	}
-	
+
 	function inline_edit_css() {
 		wp_enqueue_style('series-multiples-inline-edit');
 	}
-	
+
 	function wp_series_checklist( $post_id = 0, $args = array() ) {
 		$sp = array();
 		$defaults = array(
@@ -196,37 +196,37 @@ class osMulti {
 			'id' => '',
 			'class' => 'postform'
 		);
-		
+
 		$args = wp_parse_args($args, $defaults);
 		extract( $args, EXTR_SKIP );
 
 		$post_id = (int) $post_id;
-		
-		if ( empty($walker) || !is_a($walker, 'Walker') ) 
+
+		if ( empty($walker) || !is_a($walker, 'Walker') )
 			$walker = new Walker_SeriesChecklist;
-		
+
 		$tax = get_taxonomy($taxonomy);
 		$args['disabled'] = !current_user_can($tax->cap->assign_terms);
-		
-		
+
+
 		$args['selected_series'] = (array) $selected_series;
-		
-		
+
+
 		if ( $post_id ) {
 			$args['selected_series'] = wp_get_object_terms($post_id, $taxonomy, array_merge($args, array('fields' => 'ids')));
-		
+
 		} else {
 			$args['selected_series'] = array();
 		}
-			
-		
+
+
 		if ( is_array( $popular_series ) )
 			$args['popular_series'] = $popular_series;
 		else
 			$args['popular_series'] = get_terms( $taxonomy, array( 'fields' => 'ids', 'orderby' => 'count', 'order' => 'DESC', 'number' => 10, 'hierarchical' => false ) );
-			
+
 		$series = (array) get_terms($taxonomy, array('get' => 'all'));
-		
+
 		//need to send an array of series_parts to the assembler as well.
 		if ( !empty($post_id) ) {
 			foreach ($series as $ser) {
@@ -235,10 +235,10 @@ class osMulti {
 			}
 		} else {
 			$sp = array();
-		}	
-		
+		}
+
 		$args['series_parts'] = $sp;
-					
+
 		if ( $checked_ontop ) {
 			//Post process $series rather than adding an exclude to the get_terms() query to keep the query the same across all posts (for any query cache)
 			$checked_series = array();
@@ -249,15 +249,15 @@ class osMulti {
 					unset( $series[$k] );
 				}
 			}
-			
+
 			// Put checked series on top
 			echo call_user_func_array(array(&$walker, 'walk'), array($checked_series, 0, $args));
 		}
-		
+
 		//Then the rest of them
 		echo call_user_func_array(array(&$walker, 'walk'), array($series, 0, $args));
 	}
-	
+
 	function sort_series($where) {
 		global $wpdb;
 		$series = get_query_var(SERIES_QUERYVAR);
@@ -266,10 +266,10 @@ class osMulti {
 		$where = $wpdb->prepare(" AND orgmeta.meta_key = %s ", $part_key);
 		return $where;
 	}
-	
+
 	function ajax_series() {
 		$response = array();
-	
+
 		if ( !current_user_can( 'manage_series' ) )
 			$response['error'] = __('Sorry but you don\'t have permission to add series', 'organize-series');
 
@@ -302,8 +302,8 @@ class osMulti {
 		}
 		echo json_encode($response);
 		exit();
-	} 
-	
+	}
+
 	function delete_series($series_ID, $taxonomy_id) {
 		global $wpdb;
 		seriesicons_delete($series_ID);
@@ -311,16 +311,16 @@ class osMulti {
 		$wpdb->query( $wpdb->prepare( "DELETE FROM $wpdb->postmeta WHERE meta_key LIKE %s", $series_part) );
 		return;
 	}
-	
+
 	function meta_box() {
 		$posttypes = apply_filters('orgseries_posttype_support', array('post') );
 		foreach ($posttypes as $posttype) {
 			remove_meta_box('seriesdiv', $posttype, 'side'); //remove default meta box included with Organize Series Core plugin
-			add_meta_box('newseriesdiv', __('Series', 'organize-series-multiples'), array(&$this, 'add_meta_box'), 
+			add_meta_box('newseriesdiv', __('Series', 'organize-series-multiples'), array(&$this, 'add_meta_box'),
 				$posttype, 'side');
 		}
 	}
-	
+
 	function add_meta_box() {
 		global $post, $postdata, $content;
 		$id = isset($post) ? $post->ID : $postdata->ID;
@@ -334,7 +334,7 @@ class osMulti {
 		<ul id="serieschecklist" class="list:series serieschecklist form-no-clear">
 				<?php $this->wp_series_checklist($post->ID); ?>
 		</ul>
-		
+
 			<p id="part-description"><?php _e('The part this post is in a series is listed next to each series name.  If you select a series and leave the part number blank then the post will automatically be appended to the end of the series.', 'organize-series-multiples'); ?></p></br>
 			<strong> <?php _e('Post title in widget:', 'organize-series'); ?></strong>
 		<p id="part-description"><?php _e('A "short" title of the post that will be used in the series widget. [Leave blank to use a full title]', 'organize-series'); ?></p>
@@ -342,14 +342,14 @@ class osMulti {
 		<input type="hidden" name="is_series_save" value="1" />
 		<?php
 	}
-	
+
 	function posts_custom_column_action($column_name, $id) {
 		$seriesid = null;
 		$series_part = null;
 		$series_name = null;
 		$post_types = apply_filters('orgseries_posttype_support', array('post'));
 		if ($column_name == 'series') {
-			$column_content = "<div class=\"series_column\">\n";	
+			$column_content = "<div class=\"series_column\">\n";
 			if ( $series = get_the_series($id, false) ) {
 				$column_content .= "\t<ul class=\"series-column\">\n";
 				foreach ( $series as $ser ) {
@@ -359,7 +359,7 @@ class osMulti {
 					$series_link = get_series_link($series_name);
 					$count = $ser->count;
 					$column_content .= "\t\t<li class=\"series-column-li\">";
-					
+
 					$draft_posts = get_posts( array(
 						'post_type'	=> $post_types,
 						'post_status' => array('draft', 'future', 'pending'),
@@ -372,31 +372,31 @@ class osMulti {
 						$all_serie_posts = $count_draft_posts+$count;
 						$drafts_included = " ($all_serie_posts)";
 					}
-                                        
+
 					if ( get_post_status($id) == 'publish') {
 						$column_content .= sprintf(__('<a href="%1$s" title="%2$s">%3$s</a> %4$s of %5$s%6$s', 'organize-series-multiples'), $series_link, $series_name, $series_name, $series_part, $count, $drafts_included);
-						
+
 					} else {
 						$column_content .= sprintf(__('<a href="%1$s" title="%2$s">%3$s</a> (Currently as %4$s)', 'organize-series-multiples'), $series_link, $series_name, $series_name, $series_part);
 					}
-					
+
 					$column_content .= "</li>\n";
 				}
-				
+
 				$column_content .= "\t</ul>\n";
 				$seriesids = implode(',', $id_arr);
 				$series_names = implode(',', $sname_arr);
 				//$series_parts = implode(',', $spart_arr);
 				$column_content .= "\t" . '<div class="hidden" id="inline_series_' . $id . '">';
 				$column_content .= "\n\t\t" . '<div class="series_inline_edit">' . $seriesids . '</div>';
-				
+
 				for ($i=0; $i < count($id_arr); $i++ ) {
 					$column_content .= "\n\t\t" . '<div class="series_inline_part_'.$id_arr[$i].'">' . $spart_arr[$i] . '</div>';
 				}
 				$column_content .= "\n\t\t" . '<div class="series_post_id">' . $id . '</div>';
 				$column_content .= "\n\t\t" . '<div class="series_inline_name">' . $series_names . '</div>';
 				$column_content .= "\n\t</div>\n";
-				
+
 			} else {
 				$column_content .= "\t" . '<div class="hidden" id="inline_series_' . $id . '">';
 				$column_content .= "\n\t\t" . '<div class="series_inline_edit"></div>';
@@ -405,24 +405,24 @@ class osMulti {
 				$column_content .= "\n\t\t" . '<div class="series_inline_name"></div>';
 				$column_content .= "\n\t</div>\n";
 			}
-			
+
 			$column_content .= "</div>";
 			echo $column_content;
-		} 
+		}
 	}
 
 } //end class osMulti
 } //end class check
 
-/** 
+/**
 * Directly taken and modified from WordPress -> Walker_Category_Checklist.  Needed to do this so I can insert the fields for setting the series part when there are multiple series.
 */
 class Walker_SeriesChecklist extends Walker {
 	var $tree_type = 'series';
 	var $db_fields = array ('parent' => 'parent', 'id' => 'term_id');
 	var $alt = 0;
-	
-	function start_el(&$output, $series, $depth, $args) {
+
+	function start_el(&$output, $series, $depth = 0, $args = array(), $current_object_id = 0) {
 		extract($args);
 		$class = 'series-li ';
 		$class .= in_array( $series->term_id, $popular_series ) ? 'popular-category' : '';
